ac519ca386b9ad834c1c3c965b13f7780e0b2331
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-06-08 21:05:45 +0200

    feat(services): add getArchived method to animal service

--- src/services/animal.service.js
+++ src/services/animal.service.js
@@ -2,71 +2,49 @@ import api from './api';
 
 const animalService = {
 
-    /**
-     * Récupérer tous les animaux actifs du user
-     * @returns {object} - { count, animals[] }
-     */
+    // Récupérer tous les animaux actifs du user
     getAll: async () => {
         const response = await api.get('/animals');
         return response.data;
     },
 
-    /**
-     * Récupérer un animal par son id
-     * @param {string} id
-     * @returns {object} - animal complet
-     */
+    // Récupérer un animal par son id
     getById: async (id) => {
         const response = await api.get(`/animals/${id}`);
         return response.data;
     },
 
-    /**
-     * Créer un nouvel animal
-     * @param {object} data - { name, species, breed, gender, ... }
-     * @returns {object}    - animal créé
-     */
+    // Créer un nouvel animal
     create: async (data) => {
         const response = await api.post('/animals', data);
         return response.data;
     },
 
-    /**
-     * Modifier un animal
-     * @param {string} id
-     * @param {object} data - champs à modifier
-     * @returns {object}    - animal mis à jour
-     */
+    // Modifier un animal
     update: async (id, data) => {
         const response = await api.put(`/animals/${id}`, data);
         return response.data;
     },
 
-    /**
-     * Archiver un animal
-     * @param {string} id
-     * @returns {object} - { message }
-     */
+    // Archiver un animal
     delete: async (id) => {
         const response = await api.delete(`/animals/${id}`);
         return response.data;
     },
 
-    /**
-     * Supprimer définitivement un animal et toutes ses données
-     * @param {string} id
-     * @returns {object} - { message }
-     */
+    // Récupère les animaux archivés du user
+    getArchived: async () => {
+        const response = await api.get('/animals/archived');
+        return response.data;
+    },
+
+    // Supprimer définitivement un animal et toutes ses données
     permanentDelete: async (id) => {
         const response = await api.delete(`/animals/${id}/permanent`);
         return response.data;
     },
 
-    /**
-     * Restaurer un animal archivé
-     * @param {string} id
-     * @returns {object} - { message, animal }
-     */
+    // Restaurer un animal archivé
     restore: async (id) => {
         const response = await api.patch(`/animals/${id}/restore`);
         return response.data;
